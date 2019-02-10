6820fb968606fc4c25bae3bb534f8e45de77e74b
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-02-10 23:52:39 +0530

    Pending.

--- IITJHealthCare/HealthCentre/views.py
+++ IITJHealthCare/HealthCentre/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Welcome to the Health Centre!")
+
+    # return HttpResponse("Welcome to the Health Centre!")
+    return render(request,"HealthCentre/index.html")
