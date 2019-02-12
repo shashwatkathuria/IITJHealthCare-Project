9370e87dc6f605cd0965c200f03e564f0f4ba4be
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-02-12 19:14:29 +0530

    Added names for urls for referencing.

--- IITJHealthCare/HealthCentre/urls.py
+++ IITJHealthCare/HealthCentre/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index),
-    path('register',views.register),
-    path('doctors',views.doctors),
-    path('login',views.login),
-    path('emergency',views.emergency)
+    path('',views.index, name="index"),
+    path('register',views.register, name="register"),
+    path('doctors',views.doctors, name="doctors"),
+    path('login',views.login, name="login"),
+    path('emergency',views.emergency, name="emergency")
 ]
