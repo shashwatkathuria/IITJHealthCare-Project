4e39b552a750bce46ab55b38a3fe4cedb2e87892
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-02-12 19:15:14 +0530

    Added admin functionality for database.Added a superuser

--- IITJHealthCare/HealthCentre/admin.py
+++ IITJHealthCare/HealthCentre/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Doctor, Patient, Prescription
 # Register your models here.
+
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Prescription)
