c704930de0980eb3d4d15b1e3d8a4a990c689dea
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-02-13 23:48:09 +0530

    Added url for logout functionality.

--- IITJHealthCare/HealthCentre/urls.py
+++ IITJHealthCare/HealthCentre/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name="index"),
-    path('register',views.register, name="register"),
-    path('doctors',views.doctors, name="doctors"),
-    path('login',views.login, name="login"),
-    path('emergency',views.emergency, name="emergency")
+    path('', views.index, name = "index"),
+    path('register', views.register, name = "register"),
+    path('doctors', views.doctors, name = "doctors"),
+    path('login', views.login, name = "login"),
+    path('emergency', views.emergency, name = "emergency"),
+    path('logout', views.logout, name = "logout")
 ]
