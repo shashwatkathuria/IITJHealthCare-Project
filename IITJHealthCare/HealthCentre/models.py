d60d1952e25050fbe3e9ec06b31811c2cc909404
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-02-13 23:49:27 +0530

    Alter certain fields and added timestamp for prescriptions.

--- IITJHealthCare/HealthCentre/models.py
+++ IITJHealthCare/HealthCentre/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class Doctor(models.Model):
     name = models.CharField(max_length = 30)
     address = models.CharField(max_length = 100)
-    contactNumber = models.CharField(max_length = 10)
-    email = models.CharField(max_length = 30)
+    contactNumber = models.CharField(unique = True, max_length = 10)
+    email = models.EmailField(unique = True, max_length = 255)
 
     def __str__(self):
         return "Name : " + self.name + " Address : " + self.address + " Contact : " + self.contactNumber + " Email : " + self.email
@@ -15,8 +15,8 @@ class Patient(models.Model):
     name = models.CharField(max_length = 30)
     address = models.CharField(max_length = 100)
     contactNumber = models.CharField(max_length = 10)
-    rollNumber = models.CharField(max_length = 8)
-    email = models.CharField(max_length = 30)
+    rollNumber = models.CharField(unique = True, max_length = 8)
+    email = models.EmailField(unique = True, max_length = 255)
     password = models.CharField(max_length = 64)
 
     def __str__(self):
@@ -26,6 +26,7 @@ class Prescription(models.Model):
     prescriptionText = models.CharField(max_length = 2000)
     doctor = models.ForeignKey(Doctor, related_name = "doctorRecords", on_delete = models.CASCADE)
     patient = models.ForeignKey(Patient, related_name = "patientRecords", on_delete = models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
         return "\nDoctor :" + str(self.doctor) + "\n\nPatient :" + str(self.patient) + "\n\nPrescription : \n\n" + self.prescriptionText + "\n\n"
