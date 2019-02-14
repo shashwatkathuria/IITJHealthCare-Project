39e2faab04ce10260ee0566001d3e6bb2e543a1c
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-02-14 13:27:11 +0530

    Added number of new prescription,new view for contact us and some minor changes.

--- IITJHealthCare/HealthCentre/views.py
+++ IITJHealthCare/HealthCentre/views.py
@@ -5,18 +5,22 @@ from django.urls import reverse
 from .models import Doctor, Patient, Prescription
 from Crypto.Hash import SHA256
 from codecs import encode,decode
+from django.db.models import Count, Q
 
 # Create your views here.
 
 def index(request):
-    # if request.user.is_authenticated
+
     return render(request,"HealthCentre/index.html")
 
 def register(request):
+
     if request.method == "GET":
+
         return render(request,"HealthCentre/register.html")
 
     elif request.method == "POST":
+
         userFirstName = request.POST["userFirstName"]
         userLastName = request.POST["userLastName"]
         userEmail = request.POST["userEmail"]
@@ -25,25 +29,32 @@ def register(request):
         userContactNo = request.POST["userContactNo"]
         userPassword = request.POST["userPassword"]
         userConfirmPassword = request.POST["userConfirmPassword"]
+
         if userPassword == userConfirmPassword:
+
             name = userFirstName + " " + userLastName
+
             SHA256Engine = SHA256.new()
             userPassword = userPassword.encode()
             SHA256Engine.update(userPassword)
             passwordHash = SHA256Engine.digest()
             passwordHash = encode(passwordHash, 'hex')
             passwordHash = decode(passwordHash, 'utf-8')
+
             patient = Patient(name = name,rollNumber = userRollNo, email = userEmail, password = passwordHash, address = userAddress, contactNumber = userContactNo )
             patient.save()
+
             context = {
                 "message":"User Registration Successful. Please Login."
             }
+
             return render(request, "HealthCentre/register.html",context)
 
         else:
             context = {
                 "message":"Passwords do not match.Please register again."
             }
+
             return render(request,"HealthCentre/register.html",context)
     else:
         return render(request,"HealthCentre/register.html")
@@ -59,22 +70,36 @@ def login(request):
     if request.method == "GET":
         try:
             if request.session['isLoggedIn']:
+
                 patient = Patient.objects.get(email = request.session['userEmail'])
+
+                records = patient.patientRecords.all()
+                numberNewPrescriptions = patient.patientRecords.aggregate(newPrescriptions = Count('pk', filter = Q(new = True)))['newPrescriptions']
+                request.session['numberNewPrescriptions'] = numberNewPrescriptions
+                for record in records:
+                    record.new = False
+                    record.save()
+
                 context = {
                     "message" : "Successfully Logged In.",
                     "isAuthenticated" : True,
-                    "user": patient.patientRecords.all()
+                    "user": records.order_by('-timestamp')
                     }
+
                 return render(request,"HealthCentre/userPatientInfo.html", context)
             else:
                 return render(request,"HealthCentre/login.html")
         except:
             return render(request,"HealthCentre/login.html", context)
+
     elif request.method == "POST":
+
         userName = request.POST["useremail"]
         userPassword = request.POST["userpassword"]
+
         try:
             patient = Patient.objects.get(email = userName)
+
         except Patient.DoesNotExist:
             context = {
                 "message":"User does not exist.Please register first."
@@ -87,31 +112,82 @@ def login(request):
         passwordHash = SHA256Engine.digest()
         passwordHash = encode(passwordHash, 'hex')
         passwordHash = decode(passwordHash, 'utf-8')
+
+        records = patient.patientRecords.all()
+        numberNewPrescriptions = patient.patientRecords.aggregate(newPrescriptions = Count('pk', filter = Q(new = True)))['newPrescriptions']
+        request.session['numberNewPrescriptions'] = numberNewPrescriptions
+        for record in records:
+            record.new = False
+            record.save()
+
         if passwordHash == patient.password:
+
             context = {
                 "message" : "Successfully Logged In.",
                 "isAuthenticated" : True,
-                "user": patient.patientRecords.all()
+                "user": records.order_by('-timestamp')
             }
             request.session['isLoggedIn'] = True
             request.session['userEmail'] = userName
-            return render(request,"HealthCentre/userPatientInfo.html", context)
-        context = {
-            "message":"Invalid Credentials.Please Try Again."
-        }
-        return render(request,"HealthCentre/login.html", context)
+            request.session['Name'] = patient.name
 
+            return render(request,"HealthCentre/userPatientInfo.html", context)
+
+        else:
+
+            context = {
+                "message":"Invalid Credentials.Please Try Again."
+            }
+
+            return render(request,"HealthCentre/login.html", context)
 
     else:
         return render(request,"HealthCentre/login.html")
 
 def emergency(request):
-    return render(request,"HealthCentre/emergency.html")
+
+    if request.method == "GET":
+        return render(request,"HealthCentre/emergency.html")
+
+    elif request.method == "POST":
+
+        emergencyLocation = request.POST['emergencyLocation']
+
+        if emergencyLocation != "":
+            print("------------------------------------------------------------------------")
+            print("\n\nEMERGENCY !! AMBULANCE REQUIRED AT " + emergencyLocation + " !!\n\n")
+            print("------------------------------------------------------------------------")
+
+            context = {
+                "message" : "Ambulance reaching " + emergencyLocation + " in 2 minutes."
+            }
+
+            return render(request, "HealthCentre/emergency.html", context)
+
+        else:
+            errorMessage = "No location entered.Invalid input."
+
+            context = {
+                "message" : errorMessage
+            }
+
+            return render(request, "HealthCentre/emergency.html", context)
+
+    else:
+        return render(request,"HealthCentre/emergency.html")
 
 def logout(request):
+
     request.session['isLoggedIn'] = False
     request.session['userEmail'] = ""
+    request.session['Name'] = ""
+    request.session['numberNewPrescriptions'] = 0
+
     context = {
         "message":"Successfully Logged Out."
     }
+
     return HttpResponseRedirect(reverse('login'))
+
+def contactus(request):
+    return render(request, "HealthCentre/contactus.html")
