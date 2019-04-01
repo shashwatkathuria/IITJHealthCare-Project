0faec2c80e4c97f8d173c2f9d5cbce8601aeffda
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-04-01 19:01:09 +0530

    Started making test cases. More tests to be added.

--- IITJHealthCare/HealthCentre/tests.py
+++ IITJHealthCare/HealthCentre/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from .models import Doctor, Patient, Prescription, passwordHasher
 
-# Create your tests here.
+class DoctorsTestCase(TestCase):
+
+    def setUp(self):
+
+        passwordHash = passwordHasher("12345")
+        d1 = Doctor.objects.create(name = "Abcd Efgh", address = "Aaaa, Bbbbb, 110011", contactNumber = "8888888888", email = "abcdefgh@gmail.com", password = passwordHash)
+
+        passwordHash = passwordHasher("67890")
+        d2 = Doctor.objects.create(name = "Ijkl Mnop", address = "Cccc, Dddd, 001100", contactNumber = "9999999999", email = "ijklmnop@gmail.com", password = passwordHash)
+
+    def testDoctorCount(self):
+        doctors = Doctor.objects.all()
+        self.assertEqual(doctors.count(), 2)
+
+    def testDuplicatePasswordHashes(self):
+        doctors = Doctor.objects.all()
+        passwordHashes = []
+        for doctor in doctors:
+            passwordHashes.append(doctor.password)
+
+        passwordHashes = set(passwordHashes)
+        passwordHashes = list(passwordHashes)
+
+        self.assertEqual(len(passwordHashes), 2)
