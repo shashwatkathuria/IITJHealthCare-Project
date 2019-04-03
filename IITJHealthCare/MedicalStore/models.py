1e135a61a9790f4d27fa1b3935a492e650c52b71
Shashwat Kathuria <shashwatkathuria@yahoo.co.in>
2019-04-03 16:32:05 +0530

    Slight modification in some fields.

--- IITJHealthCare/MedicalStore/models.py
+++ IITJHealthCare/MedicalStore/models.py
@@ -3,13 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Medicine(models.Model):
-    name = models.CharField(max_length = 150)
+    name = models.CharField(unique = True, max_length = 150)
     company = models.CharField(max_length = 150)
-    expiryDate = models.DateField()
     manufacturedDate = models.DateField()
+    expiryDate = models.DateField()
     quantity = models.PositiveIntegerField()
     price = models.PositiveIntegerField()
-    photoId = models.CharField(max_length = 100)
+    photoId = models.CharField(unique = True, max_length = 100)
 
     def __str__(self):
         return "Name : " + str(self.name) + " Company : " + str(self.company) + " Manufactured Date : " + str(self.manufacturedDate) + " Expiry Date : " + str(self.expiryDate) + " Quantity : " + str(self.quantity)
