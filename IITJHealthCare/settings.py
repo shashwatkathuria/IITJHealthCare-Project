d6a3930dfcbf7b008b040042d65048e34b95c7b6
shashwatkathuria <shashwatkathuria@yahoo.co.in>
2025-04-09 00:20:54 +0530

    Modified settings.

--- IITJHealthCare/settings.py
+++ IITJHealthCare/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
-import os
+import os, dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,10 +76,10 @@ WSGI_APPLICATION = 'IITJHealthCare.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(
+        os.environ.get("AZURE_POSTGRESQL_DATABASEURL"),
+        conn_max_age = 60
+    )
 }
 
 
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
